a85d70c171f5e2bcd8efd5dcf2f7aeedda6e2ec7
Márcio Tenório <marciotenorioj@gmail.com>
2023-06-27 15:04:36 -0300

    register in discovery and have route in gateway

--- src/main/java/br/ufrn/imd/songs/SongsApplication.java
+++ src/main/java/br/ufrn/imd/songs/SongsApplication.java
@@ -1,15 +1,23 @@
 package br.ufrn.imd.songs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication()
+@SpringBootApplication
+@EnableDiscoveryClient
 public class SongsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SongsApplication.class, args);
     }
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
